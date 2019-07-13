1caf5573bd920f34801018581466d7a897dbf131
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2019-07-13 08:34:03 -0700

    Use of #macro in a program
    It is a program basically showing how to use #macro.

---
+++ use of macro.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+int main()
+{
+	float r,ar;
+	printf("enter value of r:");
+	scanf("%f",&r);
+	ar=2*PI*r;
+	printf("area of a circle is:%f",ar);
+}
